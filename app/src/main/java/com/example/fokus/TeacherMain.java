fa0a24dac47cc4a2346e2b26f8e362cf2faf8181
Philip Hu <ph27333@gmail.com>
2020-08-09 12:22:52 -0600

    Added TeacherMain/Layout, minor text editing
    Also edited a text of a button in student_main_layout from from "Classes" to "My Classes"

---
+++ app/src/main/java/com/example/fokus/TeacherMain.java
@@ -0,0 +1,144 @@
+package com.example.fokus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+
+public class TeacherMain extends AppCompatActivity {
+    private View mProgressView;
+    private View mLoginFormView;
+    private TextView tvLoad;
+
+
+    Button btnViewAssign, btnTeacherProf, btnNewAssign, btnTeacherClasses;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnViewAssign = (Button) findViewById(R.id.btnViewAssign);
+        btnTeacherClasses = (Button) findViewById(R.id.btnTeacherClasses);
+        btnNewAssign = (Button) findViewById(R.id.btnNewAssign);
+        btnTeacherProf = (Button) findViewById(R.id.btnTeacherProf);
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+        tvLoad = findViewById(R.id.tvLoad);
+
+
+
+
+        // When View Assignments is clicked
+        btnViewAssign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Takes student to the video assignment
+
+
+            }
+
+
+        });
+
+
+        // When Teacher Profile is Clicked
+        btnTeacherProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Takes student to their classes
+
+
+            }
+
+
+        });
+
+        // When My Classes is clicked
+        btnTeacherClasses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Takes student to their profile
+
+
+            }
+
+
+        });
+
+        // When New Assignment is Clicked
+        btnNewAssign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Takes student to their profile
+
+
+            }
+
+
+        });
+
+
+    }
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+
+            tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoad.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+}
