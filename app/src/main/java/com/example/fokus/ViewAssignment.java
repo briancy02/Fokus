a2842c0923a112d7d0e69d9dc84facc42c38a4d9
Philip Hu <ph27333@gmail.com>
2020-08-09 20:22:12 -0600

    All Classes added
    All barebones classes added. Video widgets on the VideoAssignment class and the NewAssignment classes

---
+++ app/src/main/java/com/example/fokus/ViewAssignment.java
@@ -0,0 +1,21 @@
+package com.example.fokus;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+public class ViewAssignment {
+}
